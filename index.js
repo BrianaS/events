140662d88d7d5d4d9886f1972e15349a42b7ad06
Briana Schiller <briana@stripe.com>
2020-03-04 17:59:58 -0800

    Events creation

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    let square = document.querySelector('#square')
+    let change = document.querySelector('#change')
+    change.addEventListener('click', function(){
+      square.style.backgroundColor = 'aqua'
+    })
+  }
